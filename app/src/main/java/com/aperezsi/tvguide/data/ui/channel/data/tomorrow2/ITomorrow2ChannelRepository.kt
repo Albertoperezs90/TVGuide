b5cc29971eec93f9cc2ab75444aebdb2a8d53dc5
Alberto Pérez <albertoperezsimon@gmail.com>
2018-06-03 23:34:48 +0200

    data for channel fragments created

---
+++ app/src/main/java/com/aperezsi/tvguide/data/ui/channel/data/tomorrow2/ITomorrow2ChannelRepository.kt
@@ -0,0 +1,11 @@
+package com.aperezsi.tvguide.data.ui.channel.data.tomorrow2
+
+import com.aperezsi.tvguide.data.data.ChannelProgamming
+import com.aperezsi.tvguide.data.data.ProgramResponse
+
+/**
+ * Created by alberto on 31/05/2018.
+ */
+interface ITomorrow2ChannelRepository {
+
+}
